feeb9bb42e1369e064f1fd0880d70bff9f665bf2
jurrian-triplence <jurrian@triplence.com>
2023-03-20 11:55:10 +0100

    Paniek form afgemaakt.

---
+++ mad-libs/paniek-result.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Onkunde</h1>
+    <?php
+        if(count(array_filter($_POST))==count($_POST)) {
+            echo 'Er heerst paniek in koninkrijk '. htmlspecialchars($_POST['3']). '. Koning '. htmlspecialchars($_POST['6']). ' is ten einde raad en als koning '. htmlspecialchars($_POST['6']). ' ten einde raad is, dan roept hij zijn ten-einde-raadsheer '. htmlspecialchars($_POST['2']). '.<br>';
+            echo '"'. htmlspecialchars($_POST['2']). ', het is een ramp! Het is een schande!"<br>';
+            echo '"Sire, Majesteit, Uwe luidruchtigheid, wat is er aan de hand?<br>';
+            echo 'Mijn '. htmlspecialchars($_POST['1']). ' is verdwenen! Zo maar, zonder waarschuwing. En ik had net '. htmlspecialchars($_POST['5']). ' voor hem gekocht!<br>';
+            echo 'Majesteit, uw '. htmlspecialchars($_POST['1']). ' komt vast vanzelf weer terug?<br>';
+            echo 'Ja das leuk en aardig, maar hoe moet ik in de tussentijd '. htmlspecialchars($_POST['7']). ' leren?<br>';
+            echo 'Maar sire, daar kunt u dan toch uw '. htmlspecialchars($_POST['8']). ' voor gebruiken?<br>';
+            echo htmlspecialchars($_POST['2']). ', je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had.<br>';
+            echo 'Mij '. htmlspecialchars($_POST['7']). ', Sire.' ;
+        } else {
+            header('Location: onkunde.php');
+        }
+    ?>
+</body>
+</html>
---
+++ mad-libs/paniek.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Onkunde</title>
+</head>
+<body>
+    <h1>Er heerst paniek</h1>
+    <div>
+        <form method="post" action="paniek-result.php">
+            <div id="inputs">
+                <p>Welk dier zou je nooit als huisdier willen hebben?</p>
+                <input type="text" name="1" required>
+            </div>
+            <div id="inputs">
+                <p>Wie is de belangrijkste persoon in je leven?</p>
+                <input type="text" name="2" required>
+            </div>
+            <div id="inputs">
+                <p>In welk land zou je graag willen wonen?</p>
+                <input type="text" name="3" required>
+            </div>
+            <div id="inputs">
+                <p>Wat doe je als je je verveelt?</p>
+                <input type="text" name="4" required>
+            </div>
+            <div id="inputs">
+                <p>Met welk speelgoed speelde je als kind het meest?</p>
+                <input type="text" name="5" required>
+            </div>
+            <div id="inputs">
+                <p>Bij welke docent spijbel je het liefst?</p>
+                <input type="text" name="6" required>
+            </div>
+            <div id="inputs">
+                <p>Als je 100.000 euro zou hebben, wat zou je dan kopen?</p>
+                <input type="text" name="8" required>
+            </div>
+            <div id="inputs">
+                <p>Wat is je favoriete bezigheid?</p>
+                <input type="text" name="7" required>
+            </div>
+            <input type="submit" value="Submit" id="submit">
+        </form>
+    </div>
+</body>
+</html>
