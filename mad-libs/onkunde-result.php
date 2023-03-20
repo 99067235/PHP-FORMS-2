aec40ba14d0f998acf4473d36a46691bb2d52a9f
jurrian-triplence <jurrian@triplence.com>
2023-03-20 11:29:42 +0100

    Onkunde form validatie gefixt

--- mad-libs/onkunde-result.php
+++ mad-libs/onkunde-result.php
@@ -9,8 +9,10 @@
 <body>
     <h1>Onkunde</h1>
     <?php
-        if ((isset($_POST['1'])) && (isset($_POST['2'])) && (isset($_POST['3'])) && (isset($_POST['4'])) && (isset($_POST['5'])) && (isset($_POST['6'])) && (isset($_POST['7']))) {
+        if(count(array_filter($_POST))==count($_POST)) {
             echo 'Er zijn veel mensen die niet kunnen '. htmlspecialchars($_POST['1']). '. Neem nou '. htmlspecialchars($_POST['2']). '. Zelfs met de hulp van een '. htmlspecialchars($_POST['4']). ' of zelfs '. htmlspecialchars($_POST['3']). ' kan '. htmlspecialchars($_POST['2']). ' niet tekenen. Dat heeft niet te maken met een gebrek aan '. htmlspecialchars($_POST['5']). ' maar met een teveel aan '. htmlspecialchars($_POST['6']). '. Te veel '. htmlspecialchars($_POST['6']). ' leidt tot '. htmlspecialchars($_POST['7']). ' en dat is niet goed als je wilt '. htmlspecialchars($_POST['1']). '. Helaas voor '. htmlspecialchars($_POST['2']). '.';
+        } else {
+            header('Location: onkunde.php');
         }
     ?>
 </body>
